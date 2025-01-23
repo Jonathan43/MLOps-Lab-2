57ba9d063167171014a1f1483f1f90898ecb19b3
Jonathan <jonathan.piscart@gmail.com>
2025-01-23 14:28:12 +0100

    Small change

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import FastAPI
-from simple_model import import_data, train_test, train_model, test_model
+from simple_model.importing_data import import_data
+from simple_model.custom_train_test import train_test
+from simple_model.model import train_model, test_model
 
 
 app = FastAPI()
