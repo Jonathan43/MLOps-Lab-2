9a8ec2447a199a03ec9da8a7dafde4c8b9bfb4b7
Jonathan <jonathan.piscart@gmail.com>
2025-01-23 15:34:29 +0100

    Still working on app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 from simple_model.importing_data import import_data
-from simple_model.custom_train_test import train_test
-from simple_model.model_inference import train_model, test_model
-
+from simple_model.model_training import train_model
 from simple_model.model_inference import prediction
+
+# Training the model
+
+dataset = import_data()
+model = train_model(dataset)
 
 app = FastAPI()
 
-
-
-
-@app.post("/predict", response_model=prediction)
+@app.post("/predict", response_model=prediction, model = model)
 def predict(data: datapoint):
     # Use the loaded model to make a prediction
     prediction = prediction(model, datapoint)
--- simple_model/model_training.py
+++ simple_model/model_training.py
@@ -4,8 +4,11 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from custom_train_test import train_test
+from custom_train_test import train_test
+from importing_data import import_data
 
-def train_model(X_train, y_train):
+def train_model(dataset):
+    X_train, X_test, y_train, y_test = train_test(dataset)
     model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
     return model
