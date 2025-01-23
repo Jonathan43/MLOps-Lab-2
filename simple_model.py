416640f58eafd062bfdfd6455bbc08161e2054a9
Jonathan <jonathan.piscart@gmail.com>
2025-01-23 13:55:17 +0100

    Simple model creation

--- simple_model.py
+++ simple_model.py
@@ -1,1 +1,27 @@
-print ('hello Leo')
+# Simple classification model for the Iris dataset
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Download and import data
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+
+
+# Train test split
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Train model
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+# Test model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
