f37d7f841c4e2ab5aa19aea5835318fdcb431604
Jonathan <jonathan.piscart@gmail.com>
2025-03-14 13:07:11 +0100

    Cleaning up the files

--- simple_model/model_training.py
+++ simple_model/model_training.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 from custom_train_test import train_test
 from importing_data import import_data
 
+
 def train_model(dataset):
     X_train, X_test, y_train, y_test = train_test(dataset)
     model = RandomForestClassifier(n_estimators=100)
